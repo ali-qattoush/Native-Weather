e6be2be9296872f2eb53eaef9482809c11cbe8fb
ali-qattoush <aliqattoushwork@gmail.com>
2024-02-12 15:10:24 +0200

    Project Initialized

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
